2b2d4d67c51cbfd28f2e1fe23f3dd51b09c91ef7
susmoy-macgill36 <43792886+susmoy-macgill36@users.noreply.github.com>
2018-12-02 03:12:03 +0600

    Create Strings.cpp
    Problem Description:
    https://www.hackerrank.com/challenges/c-tutorial-strings/problem

---
+++ C plus plus/Strings.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    string x,y,z;
+    cin>>x;
+    cin>>y;
+    int xs = x.size();
+    int ys = y.size();
+    z=x+y;
+    cout<<xs<<" "<<ys<<endl;
+    cout<<z<<endl;
+    swap(x[0],y[0]);
+    cout<<x<<" "<<y;
+
+
+
+
+
+}
