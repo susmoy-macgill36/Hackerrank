ec2b891de863dac70739894b6cc85a4ccb5bad6c
susmoy-macgill36 <43792886+susmoy-macgill36@users.noreply.github.com>
2018-12-02 03:09:01 +0600

    Create Arrays Introduction.cpp
    Problem Description:
    https://www.hackerrank.com/challenges/arrays-introduction/problem

---
+++ C plus plus/Arrays Introduction.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int n;
+ int arr[n],arr2[n];
+ vector <int> v;
+
+ cin>>n;
+ for (int i  =0; i <n ;i++)
+ {
+
+     cin>> arr[i];
+v.push_back(arr[i]);
+ }
+
+reverse(v.begin(), v.end());
+
+for(int i = 0 ; i < n ; i++)
+{
+    arr2[i] = v[i];
+
+}
+for (int i  =0; i <n ;i++)
+ {
+
+   printf("%d ",arr2[i]);
+
+
+ }
+}
