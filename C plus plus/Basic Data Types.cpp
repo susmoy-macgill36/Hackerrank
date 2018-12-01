ad5a8eced67a7a03dfc64356df16649e13de5df6
susmoy-macgill36 <43792886+susmoy-macgill36@users.noreply.github.com>
2018-12-02 02:58:00 +0600

    Create Basic Data Types.cpp
    Problem Description:
    https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem

---
+++ C plus plus/Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int a;
+    long long int  b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    printf("%.03f\n",d);
+    printf("%.09lf\n",e);
+
+}
