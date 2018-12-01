fe899059ba80cb8d8ee022984a04cfb9fda342bb
susmoy-macgill36 <43792886+susmoy-macgill36@users.noreply.github.com>
2018-12-02 03:24:05 +0600

    Create Simple Array Sum.cpp
    Problem Description:
    https://www.hackerrank.com/challenges/simple-array-sum/problem

---
+++ Problem Solving/Simple Array Sum.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    long long int array1[100000];
+    int total=0;
+    cin>>n;
+
+    for ( int i=0;i<n;i++)
+    {
+        cin>>array1[i];
+    }
+     for ( int i=0;i<n;i++)
+    {
+        total+=array1[i];
+    }
+  cout<<total;
+  return 0;
+
+}
+
