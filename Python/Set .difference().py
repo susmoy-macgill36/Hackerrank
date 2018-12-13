0f445dd2b3dd99835fec926c5c7ed4faa9a3ee50
Susmoy Chakraborty <43792886+susmoy-macgill36@users.noreply.github.com>
2018-12-13 21:28:02 +0600

    Create Set .difference().py

---
+++ Python/Set .difference().py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/py-set-difference-operation/problem
+a = int(input())
+b = set(map(int,input().strip().split(" ")))
+a1 = int(input())
+b1 = set(map(int,input().strip().split(" ")))
+print(len(b.difference(b1))
