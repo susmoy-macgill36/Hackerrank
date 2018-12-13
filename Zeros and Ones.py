bdd8aaf2969987181887d7290c0a2d94dc4af794
Susmoy Chakraborty <43792886+susmoy-macgill36@users.noreply.github.com>
2018-12-13 21:40:41 +0600

    Delete Zeros and Ones.py

--- Zeros and Ones.py
+++
@@ -1,6 +0,0 @@
-# https://www.hackerrank.com/challenges/np-zeros-and-ones/problem
-import  numpy as n
-
-dimen = tuple(map(int, input().strip().split(" ")))
-print(n.zeros(dimen,dtype="int"))
-print(n.ones(dimen,dtype="int"))
