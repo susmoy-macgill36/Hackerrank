920436ff1b9affcdf23990fc423d2cd419b426e2
Susmoy Chakraborty <43792886+susmoy-macgill36@users.noreply.github.com>
2018-12-13 21:26:49 +0600

    Create Integers Come In All Sizes.py

---
+++ Python/Integers Come In All Sizes.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+a = int(input())
+b= int(input())
+c = int(input())
+d = int(input())
+x1 = int(pow(a,b))
+x2 = int(pow(c,d))
+print(x1+x2)
